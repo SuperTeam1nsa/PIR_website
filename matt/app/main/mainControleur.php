c5e9043a10295cc2f0e539c3041eb76dcc2d5ec3
RemiF <fache@etud.insa-toulouse.fr>
2020-02-25 16:18:25 +0100

    nettoyage 2

--- matt/app/main/mainControleur.php
+++ matt/app/main/mainControleur.php
@@ -6,7 +6,6 @@ if (!isset ($_GET["action"])) {
 	die("requ&ecirc;te non autoris&eacute;e");
 }
 */
-require_once(dirname(__FILE__)."/../main/mainModele.php");
 
 class mainControleur {
 	private $modele;
@@ -23,38 +22,12 @@ class mainControleur {
 		// affichage de la vue
 		//$data = array("liste"=>$liste, "titre"=>$titre); 
         $data="";
-		$this->view("Histoire", $data,'main');
+		$this->view("principale", $data,'main');
 	}
 	public function view($view, $data = [], $module){
 		require(dirname(__FILE__).'/../'.$module.'/'.$view.'Vue.php');
 	}
-    public function showIceCreams(){
-        $data="";
-		$this->view("Glace", $data,'main');
-    }   
-	public function showForm(){
-        //check with bd if icecream available 
-        //ajax ? (pour rafraichir stock dans un 2ème temps)
-        //mail de confirmation commande qui recheck la bd (1er temps)
-        //send it in $data 
-        $data="";
-		$this->view("Form", $data,'main');
-        //action du clic : post sur url : main/checkForm
-        //la fonction checkForm check les data et envoie mail/ sms à matt
-    }   
-        
-	public function afficherFormulaire($mode, $donnees, $erreurs){
-		if($mode == "creation"){
-			$titre = "Création";
-			$action = "creer";
-		} else	if($mode == "modification"){
-			$titre = "Modification";
-			$action = "modifier";
-		}
-		// affichage de la vue
-		$data = array("donnees"=>$donnees, "erreurs"=>$erreurs, "action"=>$action, 'titre'=>$titre); 
-		$this->view("autre", $data,'accueil');
-	}
+    
 
 }
 ?>
