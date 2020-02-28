4f5a743cf76f3b14d5d1ae293813f1d3061b1cea
Remy64 <35994712+Remy64@users.noreply.github.com>
2020-02-28 15:08:03 +0100

    debut implementation form dynamique

---
+++ script/station.js
@@ -0,0 +1,23 @@
+const stations = {
+
+    riquet: 0,
+    fourier: 250,
+    exam: 40 
+
+};
+
+const vitesse = 15;
+
+document.getElementById("departure").addEventListener("change", function(e) {
+    let dest = document.getElementById("destination").value;
+    if(dest !== e.value) {
+        if
+    }
+
+
+});
+
+
+
+
+
