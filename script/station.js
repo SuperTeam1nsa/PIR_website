60b8d8a0bd312448b5a9e04a7cef9452c510ae17
Remy64 <35994712+Remy64@users.noreply.github.com>
2020-02-29 15:18:59 +0100

    calcul du temps estimé

--- script/station.js
+++ script/station.js
@@ -8,14 +8,28 @@ const stations = {
 
 const vitesse = 15;
 
-document.getElementById("departure").addEventListener("change", function(e) {
-    let dest = document.getElementById("destination").value;
-    if(dest !== e.value) {
-        if
-    }
+function estimatedTime(e) {
 
+	let dest = document.getElementById("destination").value;
+	let dep = document.getElementById("departure").value;
+	if(dep != dest) {
+		let coord_dep = stations[dep];
+		let coord_dest = stations[dest];
+		let estimatedTime = Math.abs(coord_dest - coord_dep) / (15/3.6) + 60;
+		document.getElementById("estimated_time").textContent = estimatedTime + "s";
+	}
 
-});
+	else {
+
+		document.getElementById("estimated_time").textContent = "";
+
+	}
+
+}
+
+document.getElementById("departure").addEventListener("change", estimatedTime);
+document.getElementById("destination").addEventListener("change", estimatedTime);
+
 
 
 
