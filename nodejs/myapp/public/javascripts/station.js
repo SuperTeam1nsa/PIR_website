516d5914d2bcf4242365c9d150e5181e6c567c58
Remy64 <35994712+Remy64@users.noreply.github.com>
2020-03-09 18:37:55 +0100

    amélioration estimatedTime

--- nodejs/myapp/public/javascripts/station.js
+++ nodejs/myapp/public/javascripts/station.js
@@ -1,11 +1,13 @@
-const stations = {
+const stations_num = {
 
-    riquet: 0,
-    stpi: 125,
-    fourier: 250,
-    exam: 40
+    riquet: 2,
+    stpi: 1,
+    fourier: 0,
+    exam: 3
 
 };
+
+const stations_dist = [125, 125, 110, 350]
 
 const vitesse = 15;
 
@@ -14,11 +16,18 @@ function estimatedTime(e) {
     let dest = document.getElementById("destination").value;
     let dep = document.getElementById("departure").value;
     if (dep != dest) {
-        let coord_dep = stations[dep];
-        let coord_dest = stations[dest];
-        let x = Math.abs(coord_dest - coord_dep) / (15 / 3.6) + 60;
+        let n_dep = stations_num[dep];
+        let n_dest = stations_num[dest];
+	let dst = 0;
+	
+	while(n_dep != n_dest) {
+		dst += stations_dist[n_dep];
+		n_dep = (n_dep + 1) % 4;
+		
+	}
+        let x = dst / (15 / 3.6) ;
         let min = Math.floor(x / 60);
-        let sec = Math.round(x % 60);
+        let sec = Math.floor(x % 60);
         document.getElementById("estimated_time").textContent = min + "min " + sec + "s";
     } else {
 
@@ -28,5 +37,6 @@ function estimatedTime(e) {
 
 }
 
+window.onload = estimatedTime(null);
 document.getElementById("departure").addEventListener("change", estimatedTime);
 document.getElementById("destination").addEventListener("change", estimatedTime);
